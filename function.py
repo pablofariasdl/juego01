8427c864a82015cbe1eb56e21e5cd78285e7f0d5
Pablo Farias <pablofariasdl@gmail.com>
2022-07-04 00:56:29 -0300

    Agregue el fondo de sonido y de controles

--- function.py
+++ function.py
@@ -2,8 +2,8 @@ import sys,pygame
 
 def draw(screen,adress,position):
     image = pygame.image.load(adress)
-    screen.blit(image, position)
-
+    screen.blit(image,position)
+    pygame.display.flip()
 
 
     
--- menu.py
+++ menu.py
@@ -1,16 +1,17 @@
 import pygame, sys
 import function
 
-def menu (screen,adress,ubication,stop):
+def menu (screen,adress,ubication):
+    position = 0
     
-    position = 0  #  <  >
-    screen.fill((255,255,255))
-    pygame.display.flip()
-    while not stop:
+    
+    loop = True
+
+    while loop:
+
         function.draw(screen,adress[position][0],ubication[0])
         function.draw(screen,adress[position][1],ubication[1])
         function.draw(screen,adress[position][2],ubication[2])
-        pygame.display.flip()    
 
         for event in pygame.event.get():
 
@@ -22,23 +23,48 @@ def menu (screen,adress,ubication,stop):
                         position = position -1
                 print(position)
                 if event.key == pygame.K_RETURN:
-                    if position == 0:
-                        stop = True
-                    if position == 1:
-                        sanata = True
-                        print("hacer un start pero el otro :0")
-                    if position == 2:
-                        sys.exit()
-                
+                    return position
             if event.type == pygame.QUIT:
                 sys.exit()
 
 def start (screen,stop):
+
+    
+
     adress_menu1 = [['Image/start2.png','Image/options1.png','Image/exit1.png'],
                 ['Image/start1.png','Image/options2.png','Image/exit1.png'],
                 ['Image/start1.png','Image/options1.png','Image/exit2.png']]
 
     ubication_menu1 = [(400,400),(400,480),(400,560)]
 
-    menu (screen,adress_menu1,ubication_menu1,stop)
+    adress_menu2= [['Image/controls2.png','Image/sound1.png','Image/return1.png'],
+                ['Image/controls1.png','Image/sound2.png','Image/return1.png'],
+                ['Image/controls1.png','Image/sound1.png','Image/return2.png']]
+
+    ubication_menu2 = [(400,400),(400,480),(400,560)]
+    
+    position = 0  #  <  >
+    screen.fill((255,255,255))
+    pygame.display.flip()
+
+    while not stop:
+        position = menu (screen,adress_menu1,ubication_menu1)
+        if position == 0:
+            stop = True
+        if position == 1:
+            position2 = menu (screen,adress_menu2,ubication_menu2)
+            
+            sanata = False
+            while not sanata:
+            
+                if position2 == 0:
+                    function.draw(screen,"Image/wallpaper_controls.png",(0,0))
+                    
+                if position2 == 1:
+                    function.draw(screen,"Image/wallpaper_sound.png",(0,0))
+                    
+                if position2 == 2:
+                    sanata = True
+        if position == 2:
+            sys.exit()
     
