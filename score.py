40ff285b98cad87de5c182c01316b9baf83eb121
Pablo Farias <pablofariasdl@gmail.com>
2022-07-29 00:44:13 -0300

    logre anotar el nombre con el puntaje y luego imprimirlo, falta guardarlo en el archivo y revisar todo, hay errores en la logica

--- score.py
+++ score.py
@@ -1,11 +1,63 @@
 import pygame, sys
 import function
 
+def name_up(screen,score):
+    
+    function.draw(screen,"Image/wallpaper_score.png",(0,0))
+    function.write_draw (screen,str(score),(520,80))
+    control= False
+    list_letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+                  "R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7",
+                  "8","9","0"," "]
+
+
+    list_adress=["Image/Letters/A.png","Image/Letters/B.png","Image/Letters/C.png",
+                 "Image/Letters/D.png","Image/Letters/E.png","Image/Letters/F.png",
+                 "Image/Letters/G.png","Image/Letters/H.png","Image/Letters/I.png",
+                 "Image/Letters/J.png","Image/Letters/K.png","Image/Letters/L.png",
+                 "Image/Letters/M.png","Image/Letters/N.png","Image/Letters/O.png",
+                 "Image/Letters/P.png","Image/Letters/Q.png","Image/Letters/R.png",
+                 "Image/Letters/S.png","Image/Letters/T.png","Image/Letters/U.png",
+                 "Image/Letters/V.png","Image/Letters/W.png","Image/Letters/X.png",
+                 "Image/Letters/Y.png","Image/Letters/Z.png","Image/Letters/0.png",
+                 "Image/Letters/1.png","Image/Letters/2.png","Image/Letters/3.png",
+                 "Image/Letters/4.png","Image/Letters/5.png","Image/Letters/6.png",
+                 "Image/Letters/7.png","Image/Letters/8.png","Image/Letters/9.png",
+                 "Image/Letters/0.png"]
+    position = [0,0,0]
+    ubication = [(80,80),(120,80),(160,80)]
+    x=0
+    while not control:
+        
+        for event in pygame.event.get():
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and position[x]<37:
+                    position[x]=position[x]+1
+        
+                if event.key == pygame.K_DOWN and position[x]>=0:
+                    position[x]=position[x]-1
+
+                if event.key == pygame.K_LEFT and x>=0:
+                    x=x-1
+                if event.key == pygame.K_RIGHT and x<2:
+                    x=x+1
+                if event.key == pygame.K_RETURN:
+                    control = True
+                function.draw(screen,list_adress[position[0]],ubication[0])
+                function.draw(screen,list_adress[position[1]],ubication[1])
+                function.draw(screen,list_adress[position[2]],ubication[2])
+            if event.type == pygame.QUIT:
+                sys.exit()
+    sanata=list_letters[position[0]]+list_letters[position[1]]+list_letters[position[2]]
+    return(sanata)
+
+
 def scores (screen,stop):
+    score = 3000000
+    name=name_up(screen,score)
     function.draw(screen,"Image/wallpaper_score.png",(0,0))
 
-    score = 3000000
-    
     score_list = [["AAA",999999],["AAA",999999],["AAA",999999],["AAA",999999],
                   ["AAA",999999],["AAA",999999],["AAA",999999],["AAA",999999],
                   ["AAA",999999],["AAA",999999],]
@@ -30,7 +82,7 @@ def scores (screen,stop):
     while y <= 9:
         if score > score_list[x][1] and x==y:
             score_list2[y][1]= score
-            score_list2[y][0]= "ZZZ"
+            score_list2[y][0]= name
             y= y+1
         else:
             score_list2[y][1]= score_list[x][1]
